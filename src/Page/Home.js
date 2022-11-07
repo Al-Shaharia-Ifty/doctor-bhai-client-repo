d10c966407e8459ae9293f8918485f82856ab4da
Al Shaharia <ifty682002@gmail.com>
2022-11-08 00:30:05 +0600

    navbar is working

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../Components/Banner";
 import Navbar from "../Shared/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
   );
 };
