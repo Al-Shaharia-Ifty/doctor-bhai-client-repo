81e571bcfd8fcd10c5d99050d86a62751adb5f0c
Al Shaharia <ifty682002@gmail.com>
2022-11-08 10:43:28 +0600

    react photo view download

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,7 +1,24 @@
 import React from "react";
+import banner from "../Assets/banner/banner-main.jpg";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200 px-20">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={banner} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="py-6 lg:w-2/3">
+              DocTime's experienced doctors, specialists and therapists are
+              selected carefully. Safety and quality is at the heart of
+              everything we do..
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
